216ac03a50e536b00d13b3653d9c67c62379186e
unknown <jimpidas>
2021-05-07 18:36:28 +0530

    [Jyosmita]:Added the code to calculate daily wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,13 +7,21 @@ namespace EmployeeAttandance
         static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int empHr = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
+                empHr = 8;
             else
-                Console.WriteLine("Employee is absent");
+                empHr = 0;
+
+            empWage = empHr * EMP_RATE_PER_HR;
+            Console.WriteLine("The daily wage of employee is :" + empWage);
 
         }
     }
 }
+
